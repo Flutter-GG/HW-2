2dfc778635174d7b2116d612d70810bd6d36d4e6
mrzogy <magbolmrzogy@gmail.com>
2023-08-11 01:08:13 +0300

    make delete function

--- myproject/lib/widget/card_user.dart
+++ myproject/lib/widget/card_user.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:myproject/main.dart';
 import 'package:myproject/model/my_model.dart';
 import 'package:myproject/widget/icons_widget.dart';
 
@@ -16,56 +17,61 @@ class Carduser extends StatefulWidget {
 class _CarduserState extends State<Carduser> {
   @override
   Widget build(BuildContext context) {
-    return Card(
-      margin: const EdgeInsets.all(20),
-      child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: <Widget>[
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Row(
-                children: [
-                  ClipRRect(
-                    borderRadius: BorderRadius.circular(100),
-                    child: Image.network(
-                      widget.blog.image,
-                      height: 30,
-                      width: 30,
-                      fit: BoxFit.cover,
+    return InkWell(
+      onLongPress: () {
+        listBlog.remove(widget.blog);
+      },
+      child: Card(
+        margin: const EdgeInsets.all(20),
+        child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Row(
+                  children: [
+                    ClipRRect(
+                      borderRadius: BorderRadius.circular(100),
+                      child: Image.network(
+                        widget.blog.image,
+                        height: 30,
+                        width: 30,
+                        fit: BoxFit.cover,
+                      ),
                     ),
-                  ),
-                  SizedBox(
-                    width: 10,
-                  ),
-                  Text(widget.blog.author)
-                ],
+                    SizedBox(
+                      width: 10,
+                    ),
+                    Text(widget.blog.author)
+                  ],
+                ),
               ),
-            ),
-            ListTile(
-              trailing: Image.network(
-                widget.blog.image,
-                width: 100,
-                height: 100,
+              ListTile(
+                trailing: Image.network(
+                  widget.blog.image,
+                  width: 100,
+                  height: 100,
+                ),
+                title: Text(widget.blog.title),
+                subtitle: Text(widget.blog.subject),
               ),
-              title: Text(widget.blog.title),
-              subtitle: Text(widget.blog.subject),
-            ),
-            Padding(
-              padding: const EdgeInsets.only(left: 20),
-              child: Row(
-                children: [
-                  Text(widget.blog.min),
-                ],
+              Padding(
+                padding: const EdgeInsets.only(left: 20),
+                child: Row(
+                  children: [
+                    Text(widget.blog.min),
+                  ],
+                ),
               ),
-            ),
-            SizedBox(
-              height: 10,
-            ),
-            // ListTile(
-            //   leading: Threicon(),
+              SizedBox(
+                height: 10,
+              ),
+              // ListTile(
+              //   leading: Threicon(),
 
-            // ),
-          ].toList()),
+              // ),
+            ].toList()),
+      ),
     );
   }
 }
