2659e8bb6ab00f7e1310b86d8202ab0808bbc85a
mrzogy <magbolmrzogy@gmail.com>
2023-08-10 18:54:23 +0300

    add blog class to model folder

---
+++ myproject/lib/model/my_model.dart
@@ -0,0 +1,39 @@
+class Blog {
+    String id;
+    String title;
+    DateTime date;
+    String author;
+    String content;
+    String image;
+    String subject;
+
+    Blog({
+        required this.id,
+        required this.title,
+        required this.date,
+        required this.author,
+        required this.content,
+        required this.image,
+        required this.subject,
+    });
+
+    factory Blog.fromJson(Map<String, dynamic> json) => Blog(
+        id: json["id"],
+        title: json["title"],
+        date: DateTime.parse(json["date"]),
+        author: json["author"],
+        content: json["content"],
+        image: json["image"],
+        subject: json["subject"],
+    );
+
+    Map<String, dynamic> toJson() => {
+        "id": id,
+        "title": title,
+        "date": "${date.year.toString().padLeft(4, '0')}-${date.month.toString().padLeft(2, '0')}-${date.day.toString().padLeft(2, '0')}",
+        "author": author,
+        "content": content,
+        "image": image,
+        "subject": subject,
+    };
+}
