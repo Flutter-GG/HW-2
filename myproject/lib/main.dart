5e8a0093a2d58382c1257c518889f3d8db7905ba
mrzogy <magbolmrzogy@gmail.com>
2023-08-10 18:57:59 +0300

    add list contains to class

--- myproject/lib/main.dart
+++ myproject/lib/main.dart
@@ -1,17 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:myproject/model/my_model.dart';
 import 'package:myproject/view/homeview.dart';
 
 void main() {
   runApp(const MainApp());
 }
 
+List<Blog> listBlog = [];
+
 class MainApp extends StatelessWidget {
   const MainApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Home()
-    );
+    return const MaterialApp(home: Home());
   }
 }
