44d4766038a0045306717a344212ac24a4bee39c
hsmulhim <79mulhim@gmail.com>
2023-08-11 13:25:19 +0300

    first commit

---
+++ home_work2/README.md
@@ -0,0 +1,3 @@
+# home_work2
+
+A new Flutter project.
---
+++ home_work2/android/app/src/main/kotlin/com/example/home_work2/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.home_work2
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
